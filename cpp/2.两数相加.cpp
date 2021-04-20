49518dfa60b88a1b5f9f373eab4b395be5587d4a
BrikeX <BrikeX@github.com>
2021-04-20 17:20:50 +0800

    Update

--- cpp/2.两数相加.cpp
+++ cpp/2.两数相加.cpp
@@ -18,7 +18,41 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* l_result = new ListNode(0);
 
+        ListNode* l_tmp = l_result;
+        
+        int sum = 0;
+
+        while (l1 || l2)
+        {
+            if (l1)
+            {
+                sum += l1->val;
+
+                l1 = l1->next;
+            }
+            
+            if (l2)
+            {
+                sum += l2->val;
+
+                l2 = l2->next;
+            }
+
+            l_tmp->next = new ListNode(sum % 10);
+            
+            sum /= 10;
+
+            l_tmp = l_tmp->next;
+        }
+
+        if (sum)
+        {
+            l_tmp->next = new ListNode(1);
+        }
+        
+        return l_result->next;
     }
 };
 // @lc code=end
