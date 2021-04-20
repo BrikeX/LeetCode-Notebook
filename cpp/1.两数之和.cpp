b8fb946899ae13eb0e0c0c51f54dee88fcf1e6d2
BrikeX <BrikeX@github.com>
2021-04-20 12:17:48 +0800

    Update

---
+++ cpp/1.两数之和.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result(2);
+
+        for (size_t i = 0; i < nums.size() - 1; i++)
+        {
+            for (size_t j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    result[0] = i;
+
+                    result[1] = j;
+
+                    return result;
+                }
+            }
+        }
+
+        result.clear();
+        
+        return result;
+    }
+};
+// @lc code=end
+
