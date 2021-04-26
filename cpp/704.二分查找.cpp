4a557aac9d47735564ed2d0c0e7987fe9e9cd162
BrikeX <gqxfk1997@126.com>
2021-04-27 00:24:59 +0800

    Update

--- cpp/704.二分查找.cpp
+++ cpp/704.二分查找.cpp
@@ -18,17 +18,17 @@ public:
         {
             mid = left + (right - left) / 2;
 
-            if (nums[mid] == target)
-            {
-                return mid;
-            }
-            else if (nums[mid] < target)
+            if (nums[mid] < target)
             {
                 left = mid + 1;
             }
             else if (nums[mid] > target)
             {
                 right = mid - 1;
+            }
+            else if (nums[mid] == target)
+            {
+                return mid;
             }
         }
         
