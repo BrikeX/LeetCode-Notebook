55e31483ef355960243bcaa3142d7a323693d772
BrikeX <gqxfk1997@126.com>
2021-05-05 17:16:48 +0800

    Hash table

--- cpp/509.斐波那契数.cpp
+++ cpp/509.斐波那契数.cpp
@@ -7,9 +7,25 @@
 // @lc code=start
 class Solution {
 public:
+    int memo(unordered_map<int, int>& hash_table, int n)
+    {
+        auto it = hash_table.find(n);
+
+        if (it != hash_table.end())
+        {
+            return it->second;
+        }
+        else
+        {
+            hash_table[n] = memo(hash_table, n-1) + memo(hash_table, n-2);
+
+            return hash_table[n];
+        }
+    }
+
     int fib(int n) {
-        // 暴力递归
-        if (n == 0)
+        // 哈希表
+        if (n < 1)
         {
             return 0;
         }
@@ -19,7 +35,22 @@ public:
             return 1;
         }
         
-        return (fib(n-1) + fib(n-2));
+        unordered_map<int, int> hash_table = {{0, 0}, {1, 1}, {2, 1}};
+
+        return memo(hash_table, n);
+        
+        // // 暴力递归
+        // if (n < 1)
+        // {
+        //     return 0;
+        // }
+        
+        // if (n == 1 || n == 2)
+        // {
+        //     return 1;
+        // }
+        
+        // return (fib(n-1) + fib(n-2));
     }
 };
 // @lc code=end
