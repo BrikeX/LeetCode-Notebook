de37957a9467eda6be78d9d29ed32d7d4de3bcb5
BrikeX <gqxfk1997@126.com>
2021-05-06 22:50:40 +0800

    Backtacking

---
+++ cpp/46.全排列.cpp
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=46 lang=cpp
+ *
+ * [46] 全排列
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void backTack(vector<int>& nums, vector<int>& track, vector<vector<int>>& result)
+    {
+        if (track.size() == nums.size())
+        {
+            result.push_back(track);
+
+            return;
+        }
+        
+        for (size_t i = 0; i < nums.size(); i++)
+        {
+            auto it = find(track.begin(), track.end(), nums[i]);
+
+            if (it != track.end())
+            {
+                continue;
+            }
+            else
+            {
+                track.push_back(nums[i]);
+
+                backTack(nums, track, result);
+
+                track.pop_back();
+            }
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        // 回溯算法
+        vector<int> track;
+
+        vector<vector<int>> result;
+
+        backTack(nums, track, result);
+
+        return result;
+    }
+};
+// @lc code=end
+
