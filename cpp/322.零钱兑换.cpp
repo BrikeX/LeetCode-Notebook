ed7a068674053be01691f741f18451bc15281993
BrikeX <gqxfk1997@126.com>
2021-05-06 20:28:03 +0800

    Dynamic Programming

--- cpp/322.零钱兑换.cpp
+++ cpp/322.零钱兑换.cpp
@@ -7,53 +7,78 @@
 // @lc code=start
 class Solution {
 public:
-    int memo(vector<int>& coins, unordered_map<int, int>& hash_table, int amount)
-    {
-        if (amount < 0)
-        {
-            return -1;
-        }
+    // int memo(vector<int>& coins, unordered_map<int, int>& hash_table, int amount)
+    // {
+    //     if (amount < 0)
+    //     {
+    //         return -1;
+    //     }
         
-        auto it = hash_table.find(amount);
+    //     auto it = hash_table.find(amount);
 
-        if (it != hash_table.end())
-        {
-            return it->second;
-        }
-        else
-        {
-            int result = INT_MAX;
+    //     if (it != hash_table.end())
+    //     {
+    //         return it->second;
+    //     }
+    //     else
+    //     {
+    //         int result = INT_MAX;
 
-            int result_tmp;
+    //         int result_tmp;
 
-            for (size_t i = 0; i < coins.size(); i++)
-            {
-                result_tmp = memo(coins, hash_table, amount - coins[i]);
+    //         for (size_t i = 0; i < coins.size(); i++)
+    //         {
+    //             result_tmp = memo(coins, hash_table, amount - coins[i]);
 
-                if (result_tmp == -1)
-                {
-                    continue;
-                }
+    //             if (result_tmp == -1)
+    //             {
+    //                 continue;
+    //             }
                 
-                result = min(result, result_tmp + 1);
-            }
+    //             result = min(result, result_tmp + 1);
+    //         }
 
-            hash_table[amount] = (result == INT_MAX) ? -1 : result;
+    //         hash_table[amount] = (result == INT_MAX) ? -1 : result;
 
-            return hash_table[amount];      
-        }
-    }
+    //         return hash_table[amount];      
+    //     }
+    // }
 
     int coinChange(vector<int>& coins, int amount) {
-        // 哈希表
+        // DP
         if (amount == 0)
         {
             return 0;
         }
         
-        unordered_map<int, int> hash_table = {{0, 0}};
+        vector<int> dp_table(amount+1, amount+1);
 
-        return memo(coins, hash_table, amount);
+        dp_table[0] = 0;
+
+        for (int i = 0; i < dp_table.size(); i++)
+        {
+            for (int j = 0; j < coins.size(); j++)
+            {
+                if (i - coins[j] < 0)
+                {
+                    continue;
+                }
+                
+                dp_table[i] = min(dp_table[i], dp_table[i - coins[j]] + 1);
+            }
+        }
+        
+        return (dp_table[amount] == amount + 1) ? -1 : dp_table[amount];
+
+        // // 哈希表
+        // if (amount == 0)
+        // {
+        //     return 0;
+        // }
+        
+        // unordered_map<int, int> hash_table = {{0, 0}};
+
+        // return memo(coins, hash_table, amount);
 
         // // 暴力递归，超时
         // if (amount == 0)
