7f2d3a1b34c66ff3e3ff6bbfdfa0d8fcb1c9d3b0
BrikeX <gqxfk1997@126.com>
2021-05-21 18:15:24 +0800

    Recursion

---
+++ cpp/25.k-个一组翻转链表.cpp
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode.cn id=25 lang=cpp
+ *
+ * [25] K 个一组翻转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        // 递归算法
+        if (!head)
+        {
+            return nullptr;
+        }
+
+        ListNode *left = head, *right = head;
+
+        for (size_t i = 0; i < k; i++)
+        {
+            if (!right)
+            {
+                return head;
+            }
+            
+            right = right->next;
+        }
+        
+        ListNode* new_head = reverse(left, right);
+
+        left->next = reverseKGroup(right, k);
+
+        return new_head;
+    }
+
+    ListNode* reverse(ListNode* left, ListNode* right)
+    {
+        ListNode* pre = nullptr;
+
+        ListNode* cur = left;
+
+        ListNode* nxt = left;
+
+        while (cur != right)
+        {
+            nxt = cur->next;
+
+            cur->next = pre;
+
+            pre = cur;
+
+            cur = nxt;
+        }
+        
+        return pre;
+    }
+};
+// @lc code=end
+
