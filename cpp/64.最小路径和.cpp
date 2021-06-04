51035755880f84b977100dcf775c9a8164a4e319
BrikeX <gqxfk1997@126.com>
2021-06-04 21:57:39 +0800

    Dynamic Programming

--- cpp/64.最小路径和.cpp
+++ cpp/64.最小路径和.cpp
@@ -10,10 +10,36 @@ public:
     vector<vector<int>> memo;
 
     int minPathSum(vector<vector<int>>& grid) {
-        // DP
-        memo.resize(grid.size(), vector<int>(grid[0].size(), -1));
+        // DP，自底向上
+        vector<vector<int>> dp_table(grid.size(), vector<int>(grid[0].size()));
 
-        return dp(grid, grid.size() - 1, grid[0].size() - 1);
+        dp_table[0][0] = grid[0][0];
+
+        for (size_t i = 1; i < grid.size(); i++)
+        {
+            dp_table[i][0] = dp_table[i - 1][0] + grid[i][0];
+        }
+        
+        for (size_t j = 1; j < grid[0].size(); j++)
+        {
+            dp_table[0][j] = dp_table[0][j - 1] + grid[0][j];
+        }
+        
+        for (size_t i = 1; i < grid.size(); i++)
+        {
+            for (size_t j = 1; j < grid[0].size(); j++)
+            {
+                dp_table[i][j] = min(dp_table[i - 1][j], dp_table[i][j - 1]) + 
+                                 grid[i][j];
+            }
+        }
+        
+        return dp_table.back().back();
+
+        // // DP，自顶向下
+        // memo.resize(grid.size(), vector<int>(grid[0].size(), -1));
+
+        // return dp(grid, grid.size() - 1, grid[0].size() - 1);
     }
 
     int dp(vector<vector<int>>& grid, int i, int j)
