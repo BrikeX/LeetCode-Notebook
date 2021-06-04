ea16437d69f33556d72b09932b464922aa0fde11
BrikeX <gqxfk1997@126.com>
2021-06-04 22:34:43 +0800

    Dynamic Programming

---
+++ cpp/174.地下城游戏.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=174 lang=cpp
+ *
+ * [174] 地下城游戏
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> memo;
+
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        // DP
+        memo.resize(dungeon.size(), vector<int>(dungeon[0].size(), -1));
+
+        return dp(dungeon, 0, 0);
+    }
+
+    int dp(vector<vector<int>>& dungeon, int i, int j)
+    {
+        if (i == dungeon.size() - 1 && j == dungeon[0].size() - 1)
+        {
+            return dungeon[i][j] >= 0 ? 1 : -dungeon[i][j] + 1;
+        }
+        
+        if (i == dungeon.size() || j == dungeon[0].size())
+        {
+            return INT_MAX;
+        }
+        
+        if (memo[i][j] != -1)
+        {
+            return memo[i][j];
+        }
+
+        int hp = min(dp(dungeon, i + 1, j), dp(dungeon, i, j + 1)) -
+                 dungeon[i][j];
+        
+        memo[i][j] = hp <= 0 ? 1 : hp;
+
+        return memo[i][j];
+    }
+};
+// @lc code=end
+
