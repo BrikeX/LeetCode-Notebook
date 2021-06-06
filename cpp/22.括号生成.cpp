84417a29a81852adde331ef4715ccaa77111ddca
BrikeX <gqxfk1997@126.com>
2021-06-06 22:14:54 +0800

    Backtracking/DFS

---
+++ cpp/22.括号生成.cpp
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=22 lang=cpp
+ *
+ * [22] 括号生成
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        // 回溯算法
+        vector<string> result;
+
+        string track;
+
+        backTracking(n, n, track, result);
+
+        return result;
+    }
+
+    void backTracking(int left, int right, string& track, vector<string>& result)
+    {
+        if (right < left)
+        {
+            return;
+        }
+        
+        if (left < 0 || right < 0)
+        {
+            return;
+        }
+        
+        if (left == 0 && right == 0)
+        {
+            result.push_back(track);
+
+            return;
+        }
+        
+        track.push_back('(');
+
+        backTracking(left - 1, right, track, result);
+
+        track.pop_back();
+
+        track.push_back(')');
+
+        backTracking(left, right - 1, track, result);
+
+        track.pop_back();
+    }
+};
+// @lc code=end
+
