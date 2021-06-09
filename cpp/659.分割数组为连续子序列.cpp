6886f64efb545e8ee70f460bb3b99ca8eb674fcd
BrikeX <gqxfk1997@126.com>
2021-06-09 23:28:46 +0800

    Hash table

---
+++ cpp/659.分割数组为连续子序列.cpp
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=659 lang=cpp
+ *
+ * [659] 分割数组为连续子序列
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        // 哈希表
+        unordered_map<int, int> freq, need;
+
+        for (auto &&num : nums)
+        {
+            freq[num]++;
+        }
+        
+        for (auto &&num : nums)
+        {
+            if (freq[num] == 0)
+            {
+                continue;
+            }
+            
+            if (need.count(num) && need[num] > 0)
+            {
+                freq[num]--;
+
+                need[num]--;
+
+                need[num + 1]++;
+            }
+            else if (freq[num] > 0 && freq[num + 1] > 0 && freq[num + 2] > 0)
+            {
+                freq[num]--;
+
+                freq[num + 1]--;
+                
+                freq[num + 2]--;
+
+                need[num + 3]++;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
+// @lc code=end
+
