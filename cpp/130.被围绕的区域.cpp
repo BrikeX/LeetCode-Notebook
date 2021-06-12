acc337086f41f8198fbc0ee46148be74cb4761a9
BrikeX <gqxfk1997@126.com>
2021-06-12 19:58:55 +0800

    DFS

--- cpp/130.被围绕的区域.cpp
+++ cpp/130.被围绕的区域.cpp
@@ -83,75 +83,129 @@ public:
 
 class Solution {
 public:
+    int row, col;
+
+    void dfs(vector<vector<char>>& board, int x, int y)
+    {
+        if (x < 0 || x >= row || y < 0 || y >= col || board[x][y] != 'O')
+        {
+            return;
+        }
+        
+        board[x][y] = '#';
+
+        dfs(board, x + 1, y);
+
+        dfs(board, x - 1, y);
+
+        dfs(board, x, y + 1);
+
+        dfs(board, x, y - 1);
+    }
+
     void solve(vector<vector<char>>& board) {
-        // 并查集算法
-        int row = board.size();
+        // DFS
+        row = board.size();
 
-        int col = board[0].size();
-
-        UF uf(row * col + 1);
-
-        int dummy = row * col;
+        col = board[0].size();
 
         for (int i = 0; i < row; i++)
         {
-            if (board[i][0] == 'O')
-            {
-                uf.unite(i * col, dummy);
-            }
-            
-            if (board[i][col - 1] == 'O')
-            {
-                uf.unite(i * col + col - 1, dummy);
-            }
+            dfs(board, i, 0);
+
+            dfs(board, i, col - 1);
         }
         
         for (int j = 0; j < col; j++)
         {
-            if (board[0][j] == 'O')
-            {
-                uf.unite(j, dummy);
-            }
-            
-            if (board[row - 1][j] == 'O')
-            {
-                uf.unite((row - 1) * col + j, dummy);
-            }
+            dfs(board, 0, j);
+
+            dfs(board, row - 1, j);
         }
         
-        vector<vector<int>> direction = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
-        for (int i = 1; i < row - 1; i++)
+        for (int i = 0; i < row; i++)
         {
-            for (int j = 1; j < col - 1; j++)
+            for (int j = 0; j < col; j++)
             {
                 if (board[i][j] == 'O')
                 {
-                    for (int k = 0; k < 4; k++)
-                    {
-                        int x = i + direction[k][0];
-
-                        int y = j + direction[k][1];
-
-                        if (board[x][y] == 'O')
-                        {
-                            uf.unite(x * col + y, i * col + j);
-                        }
-                    }
-                }
-            }
-        }
-        
-        for (size_t i = 1; i < row - 1; i++)
-        {
-            for (size_t j = 1; j < col - 1; j++)
-            {
-                if (!uf.isConnected(dummy, i * col + j))
-                {
                     board[i][j] = 'X';
                 }
+                else if (board[i][j] == '#')
+                {
+                    board[i][j] = 'O';
+                }
             }
         }
+
+        // // 并查集算法
+        // int row = board.size();
+
+        // int col = board[0].size();
+
+        // UF uf(row * col + 1);
+
+        // int dummy = row * col;
+
+        // for (int i = 0; i < row; i++)
+        // {
+        //     if (board[i][0] == 'O')
+        //     {
+        //         uf.unite(i * col, dummy);
+        //     }
+            
+        //     if (board[i][col - 1] == 'O')
+        //     {
+        //         uf.unite(i * col + col - 1, dummy);
+        //     }
+        // }
+        
+        // for (int j = 0; j < col; j++)
+        // {
+        //     if (board[0][j] == 'O')
+        //     {
+        //         uf.unite(j, dummy);
+        //     }
+            
+        //     if (board[row - 1][j] == 'O')
+        //     {
+        //         uf.unite((row - 1) * col + j, dummy);
+        //     }
+        // }
+        
+        // vector<vector<int>> direction = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        // for (int i = 1; i < row - 1; i++)
+        // {
+        //     for (int j = 1; j < col - 1; j++)
+        //     {
+        //         if (board[i][j] == 'O')
+        //         {
+        //             for (int k = 0; k < 4; k++)
+        //             {
+        //                 int x = i + direction[k][0];
+
+        //                 int y = j + direction[k][1];
+
+        //                 if (board[x][y] == 'O')
+        //                 {
+        //                     uf.unite(x * col + y, i * col + j);
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+        
+        // for (size_t i = 1; i < row - 1; i++)
+        // {
+        //     for (size_t j = 1; j < col - 1; j++)
+        //     {
+        //         if (!uf.isConnected(dummy, i * col + j))
+        //         {
+        //             board[i][j] = 'X';
+        //         }
+        //     }
+        // }
     }
 };
 // @lc code=end
