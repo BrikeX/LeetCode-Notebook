abb12ae3314b8132a1ba70f1f944060e50502f84
BrikeX <gqxfk1997@126.com>
2021-06-26 21:29:33 +0800

    Dynamic Programming

--- cpp/198.打家劫舍.cpp
+++ cpp/198.打家劫舍.cpp
@@ -8,19 +8,45 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        // DP，自底向上，内存优化
-        int dp_i = 0, dp_i_1 = 0, dp_i_2 = 0;
-
-        for (int i = nums.size() - 1; i >= 0; i--)
+        // DP，自底向上，正序
+        if (nums.empty())
         {
-            dp_i = max(dp_i_1, nums[i] + dp_i_2);
-
-            dp_i_2 = dp_i_1;
-
-            dp_i_1 = dp_i;
+            return 0;
         }
         
-        return dp_i;
+        int nums_size = nums.size();
+
+        if (nums_size == 1)
+        {
+            return nums.front();
+        }
+        
+        vector<int> dp(nums.size(), 0);
+
+        dp[0] = nums[0];
+
+        dp[1] = max(nums[0], nums[1]);
+
+        for (size_t i = 2; i < nums_size; i++)
+        {
+            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+        
+        return dp.back();
+
+        // // DP，自底向上，内存优化
+        // int dp_i = 0, dp_i_1 = 0, dp_i_2 = 0;
+
+        // for (int i = nums.size() - 1; i >= 0; i--)
+        // {
+        //     dp_i = max(dp_i_1, nums[i] + dp_i_2);
+
+        //     dp_i_2 = dp_i_1;
+
+        //     dp_i_1 = dp_i;
+        // }
+        
+        // return dp_i;
         
         // // DP，自底向上
         // vector<int> result(nums.size() + 2, 0);
