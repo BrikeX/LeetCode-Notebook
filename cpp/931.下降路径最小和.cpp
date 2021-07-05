4acc9285c9670793d7c527a5252daf5a4c2bb8d4
BrikeX <gqxfk1997@126.com>
2021-07-05 23:28:51 +0800

    Dynamic Programming

--- cpp/931.下降路径最小和.cpp
+++ cpp/931.下降路径最小和.cpp
@@ -8,34 +8,102 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        // DP，自顶向下
+        // DP，自底向上，空间优化
         int n = matrix.size();
 
         if (n == 1)
         {
-            return *min_element(matrix[0].begin(), matrix[0].end());
+            return matrix[0][0];
         }
+
+        vector<vector<int>> dp_table(2, vector<int>(matrix[0].size()));
+
+        dp_table[0] = matrix[0];
         
-        int result = INT_MAX;
-
-        memo.resize(n);
-
-        for (size_t i = 0; i < n; i++)
+        for (int i = 1; i < n; i++)
         {
-            memo[i].resize(n);
-
-            for (size_t j = 0; j < n; j++)
+            for (int j = 0; j < n; j++)
             {
-                memo[i][j] = INT_MAX;
+                if (j == 0)
+                {
+                    dp_table[1][j] = matrix[i][j] + min(dp_table[0][j], dp_table[0][j + 1]);
+                }
+                else if (j == n - 1)
+                {
+                    dp_table[1][j] = matrix[i][j] + min(dp_table[0][j], dp_table[0][j - 1]);
+                }
+                else
+                {
+                    dp_table[1][j] = matrix[i][j] + min(dp_table[0][j], min(dp_table[0][j - 1], dp_table[0][j + 1]));
+                }
             }
+
+            dp_table[0] = dp_table[1];
         }
+
+        return *min_element(dp_table[1].begin(), dp_table[1].end());
+
+        // // DP，自底向上
+        // int n = matrix.size();
+
+        // if (n == 1)
+        // {
+        //     return matrix[0][0];
+        // }
+
+        // vector<vector<int>> dp_table(matrix.size(), vector<int>(matrix[0].size()));
+
+        // dp_table[0] = matrix[0];
         
-        for (size_t j = 0; j < n; j++)
-        {
-            result = min(result, dp(matrix, n - 1, j));
-        }
+        // for (int i = 1; i < n; i++)
+        // {
+        //     for (int j = 0; j < n; j++)
+        //     {
+        //         if (j == 0)
+        //         {
+        //             dp_table[i][j] = matrix[i][j] + min(dp_table[i - 1][j], dp_table[i - 1][j + 1]);
+        //         }
+        //         else if (j == n - 1)
+        //         {
+        //             dp_table[i][j] = matrix[i][j] + min(dp_table[i - 1][j], dp_table[i - 1][j - 1]);
+        //         }
+        //         else
+        //         {
+        //             dp_table[i][j] = matrix[i][j] + min(dp_table[i - 1][j], min(dp_table[i - 1][j - 1], dp_table[i - 1][j + 1]));
+        //         }
+        //     }
+        // }
         
-        return result;
+        // return *min_element(dp_table[n - 1].begin(), dp_table[n - 1].end());
+
+        // // DP，自顶向下
+        // int n = matrix.size();
+
+        // if (n == 1)
+        // {
+        //     return *min_element(matrix[0].begin(), matrix[0].end());
+        // }
+        
+        // int result = INT_MAX;
+
+        // memo.resize(n);
+
+        // for (size_t i = 0; i < n; i++)
+        // {
+        //     memo[i].resize(n);
+
+        //     for (size_t j = 0; j < n; j++)
+        //     {
+        //         memo[i][j] = INT_MAX;
+        //     }
+        // }
+        
+        // for (size_t j = 0; j < n; j++)
+        // {
+        //     result = min(result, dp(matrix, n - 1, j));
+        // }
+        
+        // return result;
     }
 
 private:
