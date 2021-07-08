34fca018862dd535ac6ae8a940c11ab13967d173
BrikeX <gqxfk1997@126.com>
2021-07-08 09:40:49 +0800

    Dynamic Programming

--- cpp/64.最小路径和.cpp
+++ cpp/64.最小路径和.cpp
@@ -7,27 +7,27 @@
 // @lc code=start
 class Solution {
 public:
-    vector<vector<int>> memo;
-
     int minPathSum(vector<vector<int>>& grid) {
         // DP，自底向上
-        vector<vector<int>> dp_table(grid.size(), vector<int>(grid[0].size()));
+        int row = grid.size(), col = grid[0].size();
+
+        vector<vector<int>> dp_table(row, vector<int>(col));
 
         dp_table[0][0] = grid[0][0];
 
-        for (size_t i = 1; i < grid.size(); i++)
+        for (size_t i = 1; i < row; i++)
         {
             dp_table[i][0] = dp_table[i - 1][0] + grid[i][0];
         }
         
-        for (size_t j = 1; j < grid[0].size(); j++)
+        for (size_t j = 1; j < col; j++)
         {
             dp_table[0][j] = dp_table[0][j - 1] + grid[0][j];
         }
         
-        for (size_t i = 1; i < grid.size(); i++)
+        for (size_t i = 1; i < row; i++)
         {
-            for (size_t j = 1; j < grid[0].size(); j++)
+            for (size_t j = 1; j < col; j++)
             {
                 dp_table[i][j] = min(dp_table[i - 1][j], dp_table[i][j - 1]) + 
                                  grid[i][j];
@@ -42,27 +42,30 @@ public:
         // return dp(grid, grid.size() - 1, grid[0].size() - 1);
     }
 
-    int dp(vector<vector<int>>& grid, int i, int j)
-    {
-        if (i == 0 && j == 0)
-        {
-            return grid[i][j];
-        }
-        
-        if (i < 0 || j < 0)
-        {
-            return INT_MAX;
-        }
-        
-        if (memo[i][j] != -1)
-        {
-            return memo[i][j];
-        }
-        
-        memo[i][j] = min(dp(grid, i - 1, j), dp(grid, i, j - 1)) + grid[i][j];
+private:
+    // vector<vector<int>> memo;
 
-        return memo[i][j];
-    }
+    // int dp(vector<vector<int>>& grid, int i, int j)
+    // {
+    //     if (i == 0 && j == 0)
+    //     {
+    //         return grid[i][j];
+    //     }
+        
+    //     if (i < 0 || j < 0)
+    //     {
+    //         return INT_MAX;
+    //     }
+        
+    //     if (memo[i][j] != -1)
+    //     {
+    //         return memo[i][j];
+    //     }
+        
+    //     memo[i][j] = min(dp(grid, i - 1, j), dp(grid, i, j - 1)) + grid[i][j];
+
+    //     return memo[i][j];
+    // }
 };
 // @lc code=end
 
