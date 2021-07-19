39f43c9513e01522f3cea49165b426dc33b50b71
BrikeX <gqxfk1997@126.com>
2021-07-19 11:42:21 +0800

    Two Pointers

--- cpp/5.最长回文子串.cpp
+++ cpp/5.最长回文子串.cpp
@@ -9,22 +9,39 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         // 双指针算法
-        string result;
+        if (s.size() == 1)
+        {
+            return s;
+        }
+        
+        if (s.size() == 2)
+        {
+            return s[0] == s[1] ? s : s.substr(0, 1);
+        }
+
+        string result, s1, s2;
 
         for (int i = 0; i < s.size(); i++)
         {
-            string s1 = palindrome(s, i, i);
+            s1 = palindrome(s, i, i);
+            
+            s2 = palindrome(s, i, i + 1);
 
-            string s2 = palindrome(s, i, i + 1);
-
-            result = result.size() > s1.size() ? result : s1;
-
-            result = result.size() > s2.size() ? result : s2;
+            if (result.size() < s1.size())
+            {
+                result = s1;
+            }
+            
+            if (result.size() < s2.size())
+            {
+                result = s2;
+            }
         }
         
         return result;
     }
-    
+
+private:
     string palindrome(string& s, int left, int right)
     {
         while (left >= 0 && right < s.size() && s[left] == s[right])
