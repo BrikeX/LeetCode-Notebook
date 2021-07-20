52d20564d20a94fe88cea1b2fd5e5343840df081
BrikeX <gqxfk1997@126.com>
2021-07-20 17:56:57 +0800

    Adjacent Difference

--- cpp/1109.航班预订统计.cpp
+++ cpp/1109.航班预订统计.cpp
@@ -5,65 +5,84 @@
  */
 
 // @lc code=start
-class Difference
-{
-private:
-    vector<int> diff;
+// class Difference
+// {
+// private:
+//     vector<int> diff;
     
-public:
-    Difference(vector<int>& nums)
-    {
-        assert (nums.size() > 0);
+// public:
+//     Difference(vector<int>& nums)
+//     {
+//         assert (nums.size() > 0);
 
-        diff.resize(nums.size());
+//         diff.resize(nums.size());
 
-        diff[0] = nums[0];
+//         diff[0] = nums[0];
 
-        for (size_t i = 1; i < diff.size(); i++)
-        {
-            diff[i] = nums[i] - nums[i - 1];
-        }
-    }
+//         for (size_t i = 1; i < diff.size(); i++)
+//         {
+//             diff[i] = nums[i] - nums[i - 1];
+//         }
+//     }
 
-    void increment(int i, int j, int value)
-    {
-        diff[i] += value;
+//     void increment(int i, int j, int value)
+//     {
+//         diff[i] += value;
 
-        if (j + 1 < diff.size())
-        {
-            diff[j + 1] -= value;
-        }
-    }
+//         if (j + 1 < diff.size())
+//         {
+//             diff[j + 1] -= value;
+//         }
+//     }
 
-    vector<int> result(void)
-    {
-        vector<int> result(diff.size());
+//     vector<int> result(void)
+//     {
+//         vector<int> result(diff.size());
 
-        result[0] = diff[0];
+//         result[0] = diff[0];
 
-        for (size_t i = 1; i < result.size(); i++)
-        {
-            result[i] = result[i - 1] + diff[i];
-        }
+//         for (size_t i = 1; i < result.size(); i++)
+//         {
+//             result[i] = result[i - 1] + diff[i];
+//         }
         
-        return result;
-    }
-};
+//         return result;
+//     }
+// };
 
 class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
         // 差分数组
-        vector<int> nums(n);
-
-        Difference df(nums);
+        vector<int> result(n);
 
         for (auto &&booking : bookings)
         {
-            df.increment(booking[0] - 1, booking[1] - 1, booking[2]);
+            result[booking[0] - 1] += booking[2];
+
+            if (booking[1] < n)
+            {
+                result[booking[1]] -= booking[2];
+            }
         }
         
-        return df.result();
+        for (int i = 1; i < n; i++)
+        {
+            result[i] += result[i - 1];
+        }
+        
+        return result;
+
+        // vector<int> nums(n);
+
+        // Difference df(nums);
+
+        // for (auto &&booking : bookings)
+        // {
+        //     df.increment(booking[0] - 1, booking[1] - 1, booking[2]);
+        // }
+        
+        // return df.result();
     }
 };
 // @lc code=end
