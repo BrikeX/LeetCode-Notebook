e406f9b226b873a01a7bf599372a06faa4b05114
BrikeX <gqxfk1997@126.com>
2021-07-23 15:29:37 +0800

    The Sieve of Eratosthenes

--- cpp/204.计数质数.cpp
+++ cpp/204.计数质数.cpp
@@ -8,30 +8,33 @@
 class Solution {
 public:
     int countPrimes(int n) {
+        // The Sieve of Eratosthenes
+        if (n < 2)
+        {
+            return 0;
+        }
+        
         vector<bool> primes(n, true);
 
-        for (int i = 2; i * i < n; i++)
+        int result = 0;
+
+        for (int i = 2; i < n; i++)
         {
             if (primes[i])
             {
-                for (int j = i * i; j < n; j += i)
+                result++;
+
+                if ((long long) i * i < n)
                 {
-                    primes[j] = false;
+                    for (int j = i * i; j < n; j += i)
+                    {
+                        primes[j] = false;
+                    }
                 }
             }
         }
         
-        int count = 0;
-
-        for (size_t i = 2; i < n; i++)
-        {
-            if (primes[i])
-            {
-                count++;
-            }
-        }
-        
-        return count;
+        return result;
     }
 };
 // @lc code=end
