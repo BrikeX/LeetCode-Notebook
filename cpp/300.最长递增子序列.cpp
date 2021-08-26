c39286030539945e1bcd4ff11e9cf1326a2b3022
BrikeX <gqxfk1997@126.com>
2021-08-26 22:00:22 +0800

    Dynamic Programming

--- cpp/300.最长递增子序列.cpp
+++ cpp/300.最长递增子序列.cpp
@@ -13,7 +13,7 @@ public:
 
         int result = dp_table[0];
 
-        for (size_t i = 0; i < nums.size(); i++)
+        for (size_t i = 1; i < nums.size(); i++)
         {
             for (size_t j = 0; j < i; j++)
             {
