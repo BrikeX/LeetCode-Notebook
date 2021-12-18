b7b7a687c478b4ac8d48ece86192f6b0d13d4c1b
BrikeX <gqxfk1997@126.com>
2021-12-19 01:05:15 +0800

    Iteration

---
+++ cpp/21.合并两个有序链表.cpp
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=cpp
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        // 迭代算法
+        if (!list1)
+        {
+            return list2;
+        }
+        
+        if (!list2)
+        {
+            return list1;
+        }
+        
+        ListNode list_merge;
+
+        ListNode* node_ptr = &list_merge;
+
+        while (list1 && list2)
+        {
+            if (list1->val < list2->val)
+            {
+                node_ptr->next = list1;
+
+                list1 = list1->next;
+            }
+            else
+            {
+                node_ptr->next = list2;
+                
+                list2 = list2->next;
+            }
+            
+            node_ptr = node_ptr->next;
+        }
+        
+        node_ptr->next = !list1 ? list2 : list1;
+
+        return list_merge.next;
+    }
+};
+// @lc code=end
+
