eb6f7cd71020b6044ae186c103727af6ec2ebf11
BrikeX <gqxfk1997@126.com>
2021-12-19 01:11:20 +0800

    Recursion

--- cpp/21.合并两个有序链表.cpp
+++ cpp/21.合并两个有序链表.cpp
@@ -18,42 +18,64 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        // 迭代算法
+        // // 迭代算法
+        // if (!list1)
+        // {
+        //     return list2;
+        // }
+        
+        // if (!list2)
+        // {
+        //     return list1;
+        // }
+        
+        // ListNode list_merge;
+
+        // ListNode* node_ptr = &list_merge;
+
+        // while (list1 && list2)
+        // {
+        //     if (list1->val < list2->val)
+        //     {
+        //         node_ptr->next = list1;
+
+        //         list1 = list1->next;
+        //     }
+        //     else
+        //     {
+        //         node_ptr->next = list2;
+                
+        //         list2 = list2->next;
+        //     }
+            
+        //     node_ptr = node_ptr->next;
+        // }
+        
+        // node_ptr->next = !list1 ? list2 : list1;
+
+        // return list_merge.next;
+
+        // 递归算法
         if (!list1)
         {
             return list2;
         }
-        
-        if (!list2)
+        else if (!list2)
         {
             return list1;
         }
-        
-        ListNode list_merge;
-
-        ListNode* node_ptr = &list_merge;
-
-        while (list1 && list2)
+        else if (list1->val < list2->val)
         {
-            if (list1->val < list2->val)
-            {
-                node_ptr->next = list1;
+            list1->next = mergeTwoLists(list1->next, list2);
 
-                list1 = list1->next;
-            }
-            else
-            {
-                node_ptr->next = list2;
-                
-                list2 = list2->next;
-            }
-            
-            node_ptr = node_ptr->next;
+            return list1;
         }
-        
-        node_ptr->next = !list1 ? list2 : list1;
+        else
+        {
+            list2->next = mergeTwoLists(list1, list2->next);
 
-        return list_merge.next;
+            return list2;
+        }
     }
 };
 // @lc code=end
