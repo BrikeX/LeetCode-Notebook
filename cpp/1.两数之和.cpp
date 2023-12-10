41ec301f9d3d7bc91ce5ba8a95cd425d1556e3f5
BrikeX <gqxfk1997@126.com>
2023-12-10 17:50:21 +0800

    Hash table

--- cpp/1.两数之和.cpp
+++ cpp/1.两数之和.cpp
@@ -5,48 +5,39 @@
  */
 
 // @lc code=start
+
+#include <unordered_map>
+#include <vector>
+
 class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        // 哈希表
-        unordered_map<int, int> hash_table;
+ public:
+  std::vector<int> twoSum(const std::vector<int>& nums, const int target) {
+    // hash table
+    std::unordered_map<int, int> hash_table;
+    int num_size = static_cast<int>(nums.size());
 
-        for (int i = 0; i < nums.size(); i++)
-        {
-            auto it = hash_table.find(target - nums[i]);
-
-            if (it != hash_table.end())
-            {
-                return {it->second, i};
-            }
-            
-            hash_table[nums[i]] = i;
-        }
-
-        return {};        
-
-        // 暴力枚举
-        // vector<int> result(2);
-
-        // for (size_t i = 0; i < nums.size() - 1; i++)
-        // {
-        //     for (size_t j = i + 1; j < nums.size(); j++)
-        //     {
-        //         if (nums[i] + nums[j] == target)
-        //         {
-        //             result[0] = i;
-
-        //             result[1] = j;
-
-        //             return result;
-        //         }
-        //     }
-        // }
-
-        // result.clear();
-        
-        // return result;
+    for (int i = 0; i < num_size; ++i) {
+      auto it = hash_table.find(target - nums[i]);
+      if (it != hash_table.end()) {
+        return {it->second, i};
+      }
+      hash_table[nums[i]] = i;
     }
+    return {};
+
+    // brute-force
+    // std::vector<int> result(2);
+    // for (std::size_t i = 0; i < nums.size() - 1; i++) {
+    //   for (std::size_t j = i + 1; j < nums.size(); j++) {
+    //     if (nums[i] + nums[j] == target) {
+    //       result[0] = i;
+    //       result[1] = j;
+    //       return result;
+    //     }
+    //   }
+    // }
+    // result.clear();
+    // return result;
+  }
 };
 // @lc code=end
-
